1bf3179a08639a9f57c644a1f82e47ca0fe74104
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-11-04 18:41:54 -0300

    Add enviroment variables

--- backend/src/config/auth.js
+++ backend/src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-  secret: '5d018157d327a99569f995762300848e',
+  secret: process.env.APP_SECRET,
   expiresIn: '7d',
 };
--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'a5e9adc90cc2a0',
-    pass: '55e887f566034c',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
