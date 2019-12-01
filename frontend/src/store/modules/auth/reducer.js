4a03847b4a1d0785eaf8e574139e7f0d3c393e73
nakaokagabriel <gabrielnakaoka_99@hotmail.com>
2019-12-01 03:42:20 -0300

    Add frontend aplication gobarber

---
+++ frontend/src/store/modules/auth/reducer.js
@@ -0,0 +1,20 @@
+import produce from 'immer';
+
+const INITAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
+
+export default function auth(state = INITAL_STATE, action) {
+  switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS': {
+      return produce(state, draft => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
+    }
+    default:
+      return state;
+  }
+}
